7e49e0f987f5c8e7edf5063318e1813726786302
maliangcoder <228382686@qq.com>
2021-11-19 16:54:10 +0800

    update Interface

--- 基础/对象的类型—接口/Interfaces.ts
+++ 基础/对象的类型—接口/Interfaces.ts
@@ -24,8 +24,33 @@ let tom1 : Person1 = {
 console.log(tom1)
 
 // 任意属性
+/*
+* 使用[propName:string] 定义了任意属性读取string类型的值
+* */
 interface Person2{
     name:string;
     age?:number;
-    [propName:string]:any;
+    [propName:string]:string | number | undefined;
 }
+let tom3:Person2 = {
+    name:'tom',
+    gender:'male',
+}
+console.log(tom3)
+
+/*
+* 只读属性 readonly定义制度属性
+* 注意：只读的约束存在于第一次给对象赋值的时候，而不是第一次给只读属性赋值的时候
+* */
+interface Person4 {
+    readonly id:number;
+    name:string;
+    age?:number;
+    [key:string]:any;
+}
+let tom4:Person4 = {
+    id:857,
+    name:'Tom',
+    gender:'male'
+}
+// tom4.id = 9527  “id” is readonly
